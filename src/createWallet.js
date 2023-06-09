345a72c9daa46d6053d50e8410175c016d903f14
Alekssandher <94875660+LithYoshka@users.noreply.github.com>
2023-06-09 13:01:39 -0300

    Add files via upload

---
+++ src/createWallet.js
@@ -0,0 +1,33 @@
+//Importanto dependencias
+const bip32 = require('bip32')
+const bip39 =require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//define a rede
+//bitcoin - rede princpal - mainNet 
+//testnet -rede de teste
+const network = bitcoin.networks.testnet
+
+//derivação de carteiras hd
+const path = `m/49'/1'/0'/0`
+
+//criando um mnemonic 
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//carteira raiz hd
+let root = bip32.fromSeed(seed, network)
+
+//conta
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+console.log('Carteira gerada')
+console.log('Endereço: ', btcAddress)
+console.log('Chave privada: ', node.toWIF())
+console.log('Seed: ', mnemonic)
